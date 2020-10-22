6f1b9b135bcce6d4b0cb4c370fd53790a9ed637d
Lars Kroijer <lars@kroijer.com>
2020-10-22 12:52:12 +0300

    Update server.py

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        msg = 'Hello Lars! you requested %s' % (self.path)
+        msg = 'Hello Lars today is Thursday! you requested %s' % (self.path)
         self.wfile.write(msg.encode())
 
 
